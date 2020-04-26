86404d12d8966de47fe5c62b17c4b6b620a73efe
dumitral <alin-vasile.dumitru@microfocus.com>
2020-04-26 20:54:52 +0300

    Update

---
+++ precache-manifest.c0476b8c0cd7c82e206fa49bed2fd8b6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba6e99aee2a2ed6f25a71187cb1812e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "80360417e555a322709d",
+    "url": "./static/css/main.9f5b31f5.chunk.css"
+  },
+  {
+    "revision": "34749d22c83b9296b56f",
+    "url": "./static/js/2.e941c535.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.e941c535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80360417e555a322709d",
+    "url": "./static/js/main.ca488a66.chunk.js"
+  },
+  {
+    "revision": "df4c009fa8e6a084f9ba",
+    "url": "./static/js/runtime-main.5cdd316e.js"
+  },
+  {
+    "revision": "6cc64ac355b3e46bc3fc44e2b450264c",
+    "url": "./static/media/3d-ui-final.6cc64ac3.png"
+  },
+  {
+    "revision": "1f167548dcf0e90e5528558a269f899a",
+    "url": "./static/media/background.1f167548.jpg"
+  },
+  {
+    "revision": "e65279e5b18e2ac3d88f46bae2a36a04",
+    "url": "./static/media/bottom-dust.e65279e5.png"
+  },
+  {
+    "revision": "fa66745daf43182d2ebe53f83970ace4",
+    "url": "./static/media/glow.fa66745d.png"
+  },
+  {
+    "revision": "2b99a92e46a60ce9d0c3294545045e66",
+    "url": "./static/media/mem42-with-indications.2b99a92e.png"
+  },
+  {
+    "revision": "97dac0f72e8e6eb4e8997e06a5d462c3",
+    "url": "./static/media/search.97dac0f7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c7553d10202bea4ddb7c3222d466c94c.js"
+  "./precache-manifest.c0476b8c0cd7c82e206fa49bed2fd8b6.js"
 );
 
 self.addEventListener('message', (event) => {
