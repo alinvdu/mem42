51319746871d018a2ff7f8e6e474f55a31dd06a4
dumitral <alin-vasile.dumitru@microfocus.com>
2020-04-26 11:52:00 +0300

    Add build

---
+++ precache-manifest.a5260d91fafff24f394f9732553de58c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0470fe940ffed3d52983c442db217a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53ae06fdd207e508a6cb",
+    "url": "/static/css/main.63c0282b.chunk.css"
+  },
+  {
+    "revision": "c1b338558c20c2a049e3",
+    "url": "/static/js/2.668f536b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.668f536b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53ae06fdd207e508a6cb",
+    "url": "/static/js/main.d2ea0a4d.chunk.js"
+  },
+  {
+    "revision": "d1f2ed2872b2aac64b78",
+    "url": "/static/js/runtime-main.9a80f095.js"
+  },
+  {
+    "revision": "1f167548dcf0e90e5528558a269f899a",
+    "url": "/static/media/background.1f167548.jpg"
+  },
+  {
+    "revision": "e65279e5b18e2ac3d88f46bae2a36a04",
+    "url": "/static/media/bottom-dust.e65279e5.png"
+  },
+  {
+    "revision": "fa66745daf43182d2ebe53f83970ace4",
+    "url": "/static/media/glow.fa66745d.png"
+  },
+  {
+    "revision": "97dac0f72e8e6eb4e8997e06a5d462c3",
+    "url": "/static/media/search.97dac0f7.png"
+  },
+  {
+    "revision": "16602b58cd968476a93a5da55477c7e5",
+    "url": "/static/media/the-ui.16602b58.png"
+  }
+]);
