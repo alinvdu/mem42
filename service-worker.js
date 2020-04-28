4eac7e5a6c00fea6a6a79f228b359e46585af638
dumitral <alin-vasile.dumitru@microfocus.com>
2020-04-28 08:13:56 +0300

    Switch default to 3D

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.92bde1b868f4ccbfd042f94262b75ef4.js"
+  "./precache-manifest.67b9e97e3b31118178f2c3909749af7d.js"
 );
 
 self.addEventListener('message', (event) => {
